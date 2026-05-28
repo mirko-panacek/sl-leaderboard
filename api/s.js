c32baa456ad6dd570b29de2b9fb30c1a884f1785
mirko-panacek <mirko.panacek@hotmail.com>
2026-05-29 00:14:06 +0200

    Update s.js - Update SLUTS to RECEIVERS

--- api/s.js
+++ api/s.js
@@ -20,7 +20,7 @@ module.exports = (req, res) => {
             : '<tr><td colspan="3" class="empty">No players</td></tr>';
 
     res.setHeader("Content-Type", "text/html");
-    res.send(page("TOP SLUTS", rows));
+    res.send(page("TOP RECEIVERS", rows));
 };
 
 function page(title, rows) {
